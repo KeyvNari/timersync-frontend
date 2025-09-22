59ce871bf6a00fa4493b20c9df63f22306dffe16
keyvan.narimani@gmail.com <keyvan.narimani@gmail.com>
2025-09-22 19:45:08 +0200

    removed teh sidebar

--- src/layouts/dashboard/header/index.tsx
+++ src/layouts/dashboard/header/index.tsx
@@ -7,14 +7,12 @@ import { StickyHeader } from '@/components/sticky-header';
 import { CurrentUser } from './current-user';
 import { Notifications } from './notifications';
 import { SearchMenu } from './search-menu';
-import { SidebarButton } from './sidebar-button';
 import classes from './header.module.css';
 
 export function Header() {
   return (
     <StickyHeader className={classes.root}>
       <div className={classes.rightContent}>
-        <SidebarButton />
         <Link to="/" className={classes.logo}>
           <Logo />
         </Link>
@@ -23,7 +21,6 @@ export function Header() {
 
       <Group>
         <ColorSchemeToggler />
-        ()
         <Notifications />
         <CurrentUser />
       </Group>
